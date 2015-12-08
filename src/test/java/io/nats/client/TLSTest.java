ab4d34e62f2800ea0a2d2b44b998c9cfa35d613c
Larry McQueary <larry@apcera.com>
2015-12-08 16:30:08 -0700

    Added TestCasePrinterRule for prettier test output

--- src/test/java/io/nats/client/TLSTest.java
+++ src/test/java/io/nats/client/TLSTest.java
@@ -33,9 +33,12 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class TLSTest {
+	@Rule
+	public TestCasePrinterRule pr = new TestCasePrinterRule(System.out);
 
 	UnitTestUtilities util = new UnitTestUtilities();
 
