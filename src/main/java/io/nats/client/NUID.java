0d7869d1a01db216cb6b8e5867155e973307a156
Larry McQueary <larry@apcera.com>
2016-02-25 00:31:55 -0700

    Added NUID implementation

---
+++ src/main/java/io/nats/client/NUID.java
@@ -0,0 +1,153 @@
+package io.nats.client;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NUID {
+
+	final static Logger logger = LoggerFactory.getLogger(NUID.class);
+
+	// NUID needs to be very fast to generate and truly unique, all while being entropy pool friendly.
+	// We will use 12 bytes of crypto generated data (entropy draining), and 10 bytes of sequential data
+	// that is started at a pseudo random number and increments with a pseudo-random increment.
+	// Total is 22 bytes of base 36 ascii text :)
+	
+	// Constants
+	static final char[] digits 	= { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+	static final int 	base		= 36;
+	static final int 	preLen		= 12;
+	static final int 	seqLen		= 10;
+	static final long 	maxPre		= 4738381338321616896L;	// base^preLen == 36^12
+	static final long 	maxSeq		= 3656158440062976L; 	// base^seqLen == 36^10
+	static final long	minInc		= 33L;
+	static final long	maxInc		= 333L;
+	static final int 	totalLen 	= preLen + seqLen;
+	Random srand		 				= new SecureRandom();
+	Random prand		 			= new Random();
+
+	// Instance fields
+	char[] pre;
+	long seq;
+	long inc;
+	
+
+	// Global NUID
+	public static NUID globalNUID = new NUID();
+	private static Object lock = new Object();
+
+	static NUID getInstance() {
+		if (globalNUID == null) {
+			globalNUID = new NUID();
+		}
+		return globalNUID;
+	}
+	
+	public NUID () {
+		seq = nextLong(prand, maxSeq);
+		inc = minInc + nextLong(prand, maxInc-minInc);
+		pre = new char[preLen];
+		randomizePrefix();
+	}
+	
+	// Generate the next NUID string from the global locked NUID instance.
+	public static String nextGlobal() {
+		synchronized(lock) {
+			return getInstance().next();
+		}
+	}
+	
+	// Generate the next NUID string.
+	public String next() {
+		// Increment and capture.
+		seq += inc;
+		if (seq >= maxSeq) {
+			randomizePrefix();
+			resetSequential();
+		}
+
+		// Copy prefix
+		char[] b = new char[totalLen];
+		System.arraycopy(pre, 0, b, 0, preLen);
+		
+		// copy in the seq in base36.
+		int i = b.length;
+		for (long l = seq; i > preLen; l /= base) {
+			i--;
+			b[i] = digits[(int)(l%base)];
+		}
+		return new String(b);
+	}
+
+	// Resets the sequntial portion of the NUID
+	void resetSequential() {
+		seq = nextLong(prand, maxSeq);
+		inc = minInc + nextLong(prand, maxInc-minInc);
+	}
+	
+	// Generate a new prefix from random.
+	// This will drain entropy and will be called automatically when we exhaust the sequential
+	// Will panic if it gets an error from rand.Int()
+	public void randomizePrefix() {
+		long n = nextLong(srand, maxPre);
+		int i = pre.length;
+		for (long l = n; l>0; l /= base) {
+			i--;
+			pre[i] = digits[(int)(l % base)];
+		}
+	}
+
+	static long nextLong(Random rng, long n) {
+		// error checking and 2^x checking removed for simplicity.
+		long bits, val;
+		do {
+			bits = (rng.nextLong() << 1) >>> 1;
+			val = bits % n;
+		} while (bits-val+(n-1) < 0L);
+		return val;
+	}
+
+	/**
+	 * @return the pre
+	 */
+	char[] getPre() {
+		return pre;
+	}
+
+	/**
+	 * @param pre the pre to set
+	 */
+	void setPre(char[] pre) {
+		this.pre = pre;
+	}
+
+	/**
+	 * @return the seq
+	 */
+	long getSeq() {
+		return seq;
+	}
+
+	/**
+	 * @param seq the seq to set
+	 */
+	void setSeq(long seq) {
+		this.seq = seq;
+	}
+
+	/**
+	 * @return the inc
+	 */
+	long getInc() {
+		return inc;
+	}
+
+	/**
+	 * @param inc the inc to set
+	 */
+	void setInc(long inc) {
+		this.inc = inc;
+	}
+}
---
+++ src/test/java/io/nats/client/NUIDTest.java
@@ -0,0 +1,123 @@
+/**
+ * 
+ */
+package io.nats.client;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import io.nats.client.TestCasePrinterRule;
+
+@Category(UnitTest.class)
+public class NUIDTest {
+	@Rule
+	public TestCasePrinterRule pr = new TestCasePrinterRule(System.out);
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGlobalNUID() {
+		NUID n = NUID.getInstance();
+		assertNotNull(n);
+		assertNotNull("Expected prefix to be initialized", n.getPre());
+		assertEquals(NUID.preLen, n.getPre().length);
+		assertNotEquals("Expected seq to be non-zero", n.getSeq());
+	}
+	
+	@Test 
+	public void testNUIDRollover() {
+		NUID gnuid = NUID.getInstance();
+		gnuid.setSeq(NUID.maxSeq);
+		// copy
+		char[] oldPre = Arrays.copyOf(gnuid.getPre(), gnuid.getPre().length);
+		gnuid.next();
+		assertNotEquals("Expected new pre, got the old one", oldPre, gnuid.getPre());
+	}
+	
+	@Test
+	public void testNUIDLen() {
+		String nuid = new NUID().next();
+		assertEquals(String.format("Expected len of %d, got %d", NUID.totalLen, nuid.length()),
+				NUID.totalLen, nuid.length());
+	}
+	
+	@Test
+	public void testNUIDSpeed() {
+		long count = 10000000;
+		NUID n = new NUID();
+		
+		long start = System.nanoTime();
+		for (int i=0; i<count; i++) {
+			String nuid = n.next();
+		}
+		long elapsedNsec = System.nanoTime() - start;
+		System.err.printf("Average generation time for %d NUIDs was %fns\n", count, (double)elapsedNsec/count);
+		
+	}
+	
+	@Test
+	public void testGlobalNUIDSpeed() {
+		long count = 10000000;
+		NUID n = NUID.getInstance();
+		
+		long start = System.nanoTime();
+		for (int i=0; i<count; i++) {
+			String nuid = n.next();
+		}
+		long elapsedNsec = System.nanoTime() - start;
+		System.err.printf("Average generation time for %d global NUIDs was %fns\n", count, (double)elapsedNsec/count);
+		
+	}
+
+	@Test
+	public void testBasicUniqueness() {
+		int count = 1000000;
+		Map<String,Boolean> m = new HashMap<String,Boolean>(count);
+		
+		for (int i=0; i<count; i++) {
+			String n = NUID.nextGlobal();
+			if (m.get(n) != null) {
+				fail("Duplicate NUID found: " + n);
+			} else {
+				m.put(n, true);
+			}
+		}
+	}
+}
