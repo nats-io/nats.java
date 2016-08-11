46d60dc5f9372b64c72cb72c585a0d62c283e1e2
Larry McQueary <larry@apcera.com>
2016-08-11 09:18:36 -0600

    Updated script to fix maven versions plugin warnings

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,7 @@ openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml versions:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
