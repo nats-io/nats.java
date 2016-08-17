f70188613ac01bb19ddb8dd58a432eb0acb854ab
Larry McQueary <larry@apcera.com>
2016-08-17 15:00:46 -0600

    Eliminate extra test runs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,5 +13,5 @@ else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn deploy --settings .travis/settings.xml -DskipTests=true -B -U
-mvn javadoc:javadoc scm-publish:publish-scm
+mvn -Dmaven.test.skip=true clean package deploy:deploy --settings .travis/settings.xml -B -U  
+mvn scm-publish:publish-scm
