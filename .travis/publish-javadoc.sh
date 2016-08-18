c1dab08af88e0c2d55c74563fad2e0cfae8f4fa6
Larry McQueary <larry@apcera.com>
2016-08-17 21:01:09 -0600

    Correct github repo URL

--- .travis/publish-javadoc.sh
+++ .travis/publish-javadoc.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+set -o errexit
+
 # Get to the Travis build directory, configure git and clone the repo
 export TARG=${TRAVIS_BUILD_DIR}/target
 cd ${TARG}
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/nats-io/jnats gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/nats-io/jnats.git gh-pages > /dev/null
 
 # Commit and Push the Changes
 cd gh-pages
