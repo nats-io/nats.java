c8e2db293a63b203fe864b8ef5d82ee5b83a560e
Larry McQueary <larry@apcera.com>
2016-08-17 21:08:26 -0600

    Add verbose output

--- .travis/publish-javadoc.sh
+++ .travis/publish-javadoc.sh
@@ -15,4 +15,5 @@ git rm -rf .
 (cd ${TARG}/apidocs; tar cf - .) | tar xf -
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git config -l
 git push -fq origin gh-pages > /dev/null
