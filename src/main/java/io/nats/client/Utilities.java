7800ab31af3645847e45984880f21c066bcb20a1
Larry McQueary <larry@apcera.com>
2016-10-20 10:40:22 -0600

    Formatting change

--- src/main/java/io/nats/client/Utilities.java
+++ src/main/java/io/nats/client/Utilities.java
@@ -10,7 +10,7 @@ class Utilities {
 
     /**
      * Convert a string containing only ASCII characters to the equivalent byte array.
-     * 
+     *
      * @param str the string to convert.
      * @return a byte array containing the character bytes of of each character composing the input
      *         string.
@@ -26,11 +26,11 @@ class Utilities {
 
     /**
      * Copy an ASCII string to the specified offset in a byte array.
-     * 
+     *
      * This method assumes that the input string value is ASCII (single byte) encoding. Please do
      * not arbitrarily change this method without doing your research. It is currently the fastest
      * way to perform this operation as of Java 1.6.
-     * 
+     *
      * @param buffer the destination byte array
      * @param offset the buffer offset to write the string's bytes to.
      * @param str the string to copy
