b2032cf591f51a0829fdb997c6e46cdd23f9ed3e
Larry McQueary <larry@apcera.com>
2016-10-24 11:27:53 -0600

    Change test category (again) due to timeout

--- src/test/java/io/nats/client/NUIDTest.java
+++ src/test/java/io/nats/client/NUIDTest.java
@@ -76,7 +76,7 @@ public class NUIDTest {
     }
 
     @Test(timeout = 5000)
-    @Category(UnitTest.class)
+    @Category(BenchmarkTest.class)
     public void testProperPrefix() {
         char min = (char) 255;
         char max = (char) 0;
