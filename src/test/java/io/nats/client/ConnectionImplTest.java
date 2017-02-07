66477625f6be20d482b03456b449e99376382e3b
Larry McQueary <larry@apcera.com>
2017-02-07 06:47:01 -0700

    Updated Mockito import location

--- src/test/java/io/nats/client/ConnectionImplTest.java
+++ src/test/java/io/nats/client/ConnectionImplTest.java
@@ -94,7 +94,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockingDetails;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
