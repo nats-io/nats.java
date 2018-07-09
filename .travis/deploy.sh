607e856a7290cd9c85c49f691cc6dc02e4d762cd
Stephen Asbury <sasbury@synadia.com>
2018-07-09 12:56:49 -0700

    added echo to try to debug issue with sonatype deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [[ ("$TRAVIS_BRANCH" == 'master' || "$TRAVIS_BRANCH" == 'version2') && ("$TRAVIS_PULL_REQUEST" == 'false') ]]; then
+echo $SONATYPE_USERNAME
 openssl aes-256-cbc -K $encrypted_f07928735f08_key -iv $encrypted_f07928735f08_iv -in .travis/nats.travis.gpg.enc -out .travis/nats.travis.gpg -d
 ./gradlew uploadArchives -PossrhUsername=$SONATYPE_USERNAME -PossrhPassword=$SONATYPE_PASSWORD -Psigning.keyId=$GPG_KEY_ID -Psigning.password=$GPG_KEY_PASSPHRASE -Psigning.secretKeyRingFile=.travis/nats.travis.gpg
 fi
