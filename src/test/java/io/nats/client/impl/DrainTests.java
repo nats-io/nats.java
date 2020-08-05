e9bda16e188ea0222fe2c0ab4cbcbbce1fef99fd
Matthias Hanel <mh@synadia.com>
2020-08-05 14:29:23 -0400

    Remove check for timer as it is not reliable
    There where more than 1 timer with the same name when running in gradlew

--- src/test/java/io/nats/client/impl/DrainTests.java
+++ src/test/java/io/nats/client/impl/DrainTests.java
@@ -43,17 +43,13 @@ import io.nats.client.ConnectionListener.Events;
 public class DrainTests {
 
     @Test
-    public void testLeftOverTimer() throws Exception {
+    public void testCloseOnDrainFailure() throws Exception {
         try (NatsTestServer ts = new NatsTestServer(false);
             final Connection subCon = Nats.connect(new Options.Builder().server(ts.getURI()).maxReconnects(0).build())){
             assertTrue("Connected Status", Connection.Status.CONNECTED == subCon.getStatus());
 
             Subscription sub = subCon.subscribe("draintest");
             subCon.flush(Duration.ofSeconds(1)); // Get the sub to the server, so drain has things to do
-
-            assertTrue("Timer exists", Thread.getAllStackTraces().keySet().stream().anyMatch(thread -> {
-                return thread.getName().equals("Nats Connection Timer");
-            }));
 
             ts.shutdown(); // shut down the server to fail drain and subsequent close
             boolean timedOut = false;
@@ -62,16 +58,10 @@ public class DrainTests {
             } catch (java.util.concurrent.TimeoutException e) {
                 timedOut = true;
             } finally {
-                subCon.close();
-                assertFalse("Timer gone", Thread.getAllStackTraces().keySet().stream().anyMatch(thread -> {
-                    return thread.getName().equals("Nats Connection Timer");
-                }));
+                assertTrue("Expect closed connection", Connection.Status.CLOSED == subCon.getStatus());
+                subCon.close(); // this is what I'd expect common code to do
             }
             assertTrue("Expect timeout exception", timedOut);
-            assertTrue("Expect closed connection", Connection.Status.CLOSED == subCon.getStatus());
-            assertFalse("Timer gone", Thread.getAllStackTraces().keySet().stream().anyMatch(thread -> {
-                return thread.getName().equals("Nats Connection Timer");
-            }));
         }
     }
 
