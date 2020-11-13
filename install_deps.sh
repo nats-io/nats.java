a2afbcea3d1264f99dbf665902f67bf6a081674a
Colin Sullivan <colin@synadia.com>
2020-11-13 16:29:49 -0700

    test without gopath
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_deps.sh
+++ install_deps.sh
@@ -6,10 +6,10 @@ wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
 echo "Installing go."
 tar -C `pwd` -xzf tmp.tar.gz
 export PATH=`pwd`/go/bin:$PATH
-export GOPATH=`pwd`/go
+#export GOPATH=`pwd`/go
 
 echo "Getting the nats-server"
-mkdir -p $GOPATH/src/github.com/nats-io
+#mkdir -p $GOPATH/src/github.com/nats-io
 GO111MODULE=on go get golang.org/x/crypto/ed25519
 GO111MODULE=on go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
 
