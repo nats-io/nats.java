7b59cc5dd60f9aad92fea38fa9f2ae772271eeac
Colin Sullivan <colin@synadia.com>
2020-11-13 16:50:09 -0700

    simplfy build
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 echo "Downloading go."
-wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
+wget --quiet https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
 
 echo "Installing go."
 tar -C `pwd` -xzf tmp.tar.gz
 export PATH=`pwd`/go/bin:$PATH
-#export GOPATH=`pwd`/go
 
 echo "Getting the nats-server"
-#mkdir -p $GOPATH/src/github.com/nats-io
-GO111MODULE=on go get golang.org/x/crypto/ed25519
-GO111MODULE=on go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
+go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
 
 # nats-server should be in `pwd`/go/bin
-cp `pwd`/go/bin/nats-server .
+cp `pwd`/bin/nats-server .
 
 echo "NATS server version"
 `pwd`/nats-server --version
