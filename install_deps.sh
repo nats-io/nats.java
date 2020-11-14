2a0f19ceb4739c3e95677205a041b05566545e43
Colin Sullivan <colin@synadia.com>
2020-11-13 17:35:04 -0700

    more testing
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 echo "Downloading go."
-wget --quiet https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
+wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
 
 echo "Installing go."
 tar -C `pwd` -xzf tmp.tar.gz
 export PATH=`pwd`/go/bin:$PATH
 
 echo "Getting the nats-server"
-go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
+GO111MODULE=on go get golang.org/x/crypto/ed25519
+GO111MODULE=on go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
 
 # nats-server should be in `pwd`/go/bin
-cp `pwd`/bin/nats-server .
+cp $GOROOT/bin/nats-server .
 
 echo "NATS server version"
 `pwd`/nats-server --version
