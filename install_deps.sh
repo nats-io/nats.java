c511970774c868575730a0f140724e5bdbdd558c
Colin Sullivan <colin@synadia.com>
2020-11-16 15:17:01 -0700

    fix server build
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 echo "Downloading go."
-wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
+wget --quiet https://golang.org/dl/go1.15.5.linux-amd64.tar.gz -O tmp.tar.gz
 
 echo "Installing go."
 tar -C `pwd` -xzf tmp.tar.gz
-export PATH=`pwd`/go/bin:$PATH
+export GOROOT=`pwd`/go
+export PATH=$GOROOT/bin:$PATH
+
 
 echo "Getting the nats-server"
 GO111MODULE=on go get golang.org/x/crypto/ed25519
