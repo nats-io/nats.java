8350c1f0c9e6d1f4303ec45906a2a4c30deb891f
Colin Sullivan <colin@synadia.com>
2020-11-16 15:24:24 -0700

    debug script
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- install_deps.sh
+++ install_deps.sh
@@ -9,11 +9,11 @@ export GOROOT=`pwd`/go
 export PATH=$GOROOT/bin:$PATH
 
 echo "Getting the nats-server"
-GO111MODULE=on go get golang.org/x/crypto/ed25519
-GO111MODULE=on go get github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
+GO111MODULE=on go install golang.org/x/crypto/ed25519
+GO111MODULE=on go install github.com/nats-io/nats-server/v2@398ef78aac066a54867d10b73fccf50089de0d02
 
 # nats-server should be in `pwd`/go/bin
-find .
+ls $GOROOT/bin
 cp $GOROOT/bin/nats-server .
 
 echo "NATS server version"
