bf0a58b8cf1a925be772be31f55f9dab883443b6
scottf <scottfauerbach@gmail.com>
2021-03-31 17:03:14 -0400

    fixing the build (2) reverting to older gradle because travis

--- src/main/java/io/nats/client/package-info.java
+++ src/main/java/io/nats/client/package-info.java
@@ -17,6 +17,6 @@
  * use the {@link io.nats.client.Connection Connection} object to publish messages or create subscriptions.
  * 
  * @since 2.0.0
- * @version 0.0.210.1
+ * @version 0.0.210.2
  */
 package io.nats.client;
