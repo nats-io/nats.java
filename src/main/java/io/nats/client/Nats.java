6eb1df3f8ccf3d3d5d082f992eea2fd5ee78f118
scottf <scottfauerbach@gmail.com>
2021-03-31 16:39:28 -0400

    fixing the build (1)

--- src/main/java/io/nats/client/Nats.java
+++ src/main/java/io/nats/client/Nats.java
@@ -72,7 +72,7 @@ public abstract class Nats {
     /**
      * Current version of the library - {@value}
      */
-    public static final String CLIENT_VERSION = "2.10.0";
+    public static final String CLIENT_VERSION = "0.0.210.1";
 
     /**
      * Current language of the library - {@value}
--- src/main/java/io/nats/client/package-info.java
+++ src/main/java/io/nats/client/package-info.java
@@ -17,6 +17,6 @@
  * use the {@link io.nats.client.Connection Connection} object to publish messages or create subscriptions.
  * 
  * @since 2.0.0
- * @version 2.10.0
+ * @version 0.0.210.1
  */
 package io.nats.client;
