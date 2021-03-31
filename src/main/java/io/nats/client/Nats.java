a5d0664a9f057d9792abe24ffc4d2ba2d565e4c4
scottf <scottfauerbach@gmail.com>
2021-03-31 12:22:42 -0400

    2.10.2 again

--- src/main/java/io/nats/client/Nats.java
+++ src/main/java/io/nats/client/Nats.java
@@ -72,7 +72,7 @@ public abstract class Nats {
     /**
      * Current version of the library - {@value}
      */
-    public static final String CLIENT_VERSION = "2.10.2;
+    public static final String CLIENT_VERSION = "2.10.2";
 
     /**
      * Current language of the library - {@value}
