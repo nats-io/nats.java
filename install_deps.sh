0fef2d53864efbfaf8c1ff07422777b5444527ce
scottf <scottfauerbach@gmail.com>
2021-04-01 15:09:31 -0400

    fixing the build (39) keep your fingers crossed

--- install_deps.sh
+++ install_deps.sh
@@ -24,24 +24,3 @@ cp $GOROOT/bin/nats-server nats-server
 
 echo "NATS server version:"
 `pwd`/nats-server/nats-server --version
-
-
-cd $curdir
-
-#sudo apt-get update -y
-#sudo apt-get install openjdk-8-jdk -y
-#sudo update-java-alternatives --set java-1.8.0-openjdk-amd64
-
-#wget -q https://services.gradle.org/distributions/gradle-5.5.1-bin.zip -P /tmp
-#sudo unzip -d /opt/gradle /tmp/gradle-*.zip
-#export GRADLE_HOME=/opt/gradle/gradle-5.5.1
-
-#wget -q https://services.gradle.org/distributions/gradle-5.1.1-bin.zip -P /tmp
-#sudo unzip -d /opt/gradle /tmp/gradle-*.zip
-#export GRADLE_HOME=/opt/gradle/gradle-5.1.1
-
-#wget -q https://services.gradle.org/distributions/gradle-6.7-bin.zip -P /tmp
-#sudo unzip -d /opt/gradle /tmp/gradle-*.zip
-#export GRADLE_HOME=/opt/gradle/gradle-6.7
-#
-#export PATH=${GRADLE_HOME}/bin:${PATH}
--- src/main/java/io/nats/client/Nats.java
+++ src/main/java/io/nats/client/Nats.java
@@ -72,7 +72,7 @@ public abstract class Nats {
     /**
      * Current version of the library - {@value}
      */
-    public static final String CLIENT_VERSION = "0.0.210.7";
+    public static final String CLIENT_VERSION = "2.10.0";
 
     /**
      * Current language of the library - {@value}
