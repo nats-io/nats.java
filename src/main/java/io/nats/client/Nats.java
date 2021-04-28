4ca73462e66b6f3cb7e12a47f643a2704c849ed7
scottf <scottfauerbach@gmail.com>
2021-04-28 15:05:12 -0400

    putting back 2.11.0

--- src/main/java/io/nats/client/Nats.java
+++ src/main/java/io/nats/client/Nats.java
@@ -72,7 +72,7 @@ public abstract class Nats {
     /**
      * Current version of the library - {@value}
      */
-    public static final String CLIENT_VERSION = "0.0.211.1";
+    public static final String CLIENT_VERSION = "2.11.0";
 
     /**
      * Current language of the library - {@value}
