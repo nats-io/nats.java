a8b6750ff57660986d291b2a7630e03d3ca13aae
scottf <scottfauerbach@gmail.com>
2021-05-14 07:48:09 -0400

    ability to report to file and extract totals

--- src/examples/java/io/nats/examples/jsmulti/Stats.java
+++ src/examples/java/io/nats/examples/jsmulti/Stats.java
@@ -1,14 +1,18 @@
 package io.nats.examples.jsmulti;
 
+import java.io.PrintStream;
 import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
 
 public class Stats {
-    public static final String REPORT_SEP_LINE = "| -------------- | ----------------- | --------------- | ------------------------ | ---------------- |";
     public static final long HUMAN_BYTES_BASE = 1024;
     public static final String[] HUMAN_BYTES_UNITS = new String[] {"b", "kb", "mb", "gb", "tb", "pb", "eb"};
-    public static final String ZEROS = "000000";
+    public static final String ZEROS = "000000000";
+
+    public static final String REPORT_SEP_LINE    = "| --------------- | ----------------- | --------------- | ------------------------ | ---------------- |";
+    public static final String REPORT_LINE_HEADER = "|                 |             count |            time |                 msgs/sec |        bytes/sec |";
+    public static final String REPORT_LINE_FORMAT = "| %-15s | %12s msgs | %12s ms | %15s msgs/sec | %12s/sec |\n";
 
     public double elapsed = 0;
     public double bytes = 0;
@@ -33,42 +37,55 @@ public class Stats {
         count(bytes);
     }
 
+    public static void report(Stats stats) {
+        report(stats, "Total", true, true, System.out);
+    }
+
     public static void report(Stats stats, String label, boolean header, boolean footer) {
+        report(stats, label, header, footer, System.out);
+    }
+
+    public static void report(Stats stats, String label, boolean header, boolean footer, PrintStream out) {
         double elapsed = stats.elapsed / 1e6;
         double messagesPerSecond = stats.elapsed == 0 ? 0 : stats.messageCount * 1e9 / stats.elapsed;
         double bytesPerSecond = 1e9 * (stats.bytes)/(stats.elapsed);
         if (header) {
-            System.out.println("\n" + REPORT_SEP_LINE);
-            System.out.println("|                |             count |            time |                 msgs/sec |        bytes/sec |");
-            System.out.println(REPORT_SEP_LINE);
+            out.println("\n" + REPORT_SEP_LINE);
+            out.println(REPORT_LINE_HEADER);
+            out.println(REPORT_SEP_LINE);
         }
-        System.out.printf("| %-14s | %12s msgs | %12s ms | %15s msgs/sec | %12s/sec |\n", label,
+        out.printf(REPORT_LINE_FORMAT, label,
                 format(stats.messageCount),
                 format3(elapsed),
                 format3(messagesPerSecond),
                 humanBytes(bytesPerSecond));
 
         if (footer) {
-            System.out.println(REPORT_SEP_LINE);
+            out.println(REPORT_SEP_LINE);
         }
     }
 
-    public static void report(Stats stats) {
-        report(stats, "Total", true, true);
-    }
-
-    public static void report(List<Stats> statList) {
+    public static Stats total(List<Stats> statList) {
         Stats total = new Stats();
-        int x = 0;
         for (Stats stats : statList) {
-            report(stats, "Thread " + (++x), x == 1, false);
             total.elapsed = Math.max(total.elapsed, stats.elapsed);
             total.messageCount += stats.messageCount;
             total.bytes += stats.bytes;
         }
+        return total;
+    }
 
-        System.out.println(REPORT_SEP_LINE);
-        report(total, "Total", false, true);
+    public static void report(List<Stats> statList) {
+        report(statList, System.out);
+    }
+
+    public static void report(List<Stats> statList, PrintStream out) {
+        Stats total = new Stats();
+        for (int x = 0; x < statList.size(); x++) {
+            report(statList.get(x), "Thread " + (x+1), x == 0, false, out);
+        }
+        out.println(REPORT_SEP_LINE);
+        report(total, "Total", false, true, out);
     }
 
     public static String humanBytes(double bytes) {
