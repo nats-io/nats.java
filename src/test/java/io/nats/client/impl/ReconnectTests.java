02e7948d16dbd0c8774a222a991f444104e710ab
scottf <scottfauerbach@gmail.com>
2021-06-23 16:40:55 -0400

    cluster config needs cluster name

--- src/test/java/io/nats/client/impl/ReconnectTests.java
+++ src/test/java/io/nats/client/impl/ReconnectTests.java
@@ -533,7 +533,7 @@ public class ReconnectTests {
             handler.prepForStatusChange(Events.RECONNECTED);
 
             ts.close();
-            flushAndWait(nc, handler, VERY_LONG_TIMEOUT_MS, VERY_LONG_TIMEOUT_MS);
+            flushAndWaitLong(nc, handler);
             assertConnected(nc);
 
             URI uri = options.createURIForServer(nc.getConnectedUrl());
