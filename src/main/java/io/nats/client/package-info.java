c862b17b08d4a76a0da124ca73f45a2fcdcaced9
scottf <scottfauerbach@gmail.com>
2022-02-04 16:01:00 -0500

    changed package-info.java

---
+++ src/main/java/io/nats/client/package-info.java
@@ -0,0 +1,22 @@
+// Copyright 2015-2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * The Java NATS client is encapsulated into this single package {@code io.nats.client}. Applications
+ * will start from the {@link io.nats.client.Nats Nats} class to connect to the NATS server. Once connected, they can
+ * use the {@link io.nats.client.Connection Connection} object to publish messages or create subscriptions.
+ *
+ * @since 2.0.0
+ * @version 2.x.x
+ */
+package io.nats.client;
