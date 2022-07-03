c725606c3a5de0841488274a5edcbf5a5808db40
Scott Fauerbach <scottfauerbach@gmail.com>
2022-07-03 18:27:23 -0400

    experimental pull reader (#683)

--- src/main/java/io/nats/client/impl/NatsJetStreamPullSubscription.java
+++ src/main/java/io/nats/client/impl/NatsJetStreamPullSubscription.java
@@ -1,4 +1,4 @@
-// Copyright 2021 The NATS Authors
+// Copyright 2021-2022 The NATS Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at:
@@ -13,6 +13,7 @@
 
 package io.nats.client.impl;
 
+import io.nats.client.JetStreamReader;
 import io.nats.client.Message;
 import io.nats.client.PullRequestOptions;
 
@@ -276,5 +277,58 @@ public class NatsJetStreamPullSubscription extends NatsJetStreamSubscription {
                 return next;
             }
         };
+    }
+
+    static class JetStreamReaderImpl implements JetStreamReader {
+        private final NatsJetStreamPullSubscription sub;
+        private final int batchSize;
+        private final int repullAt;
+        private int currentBatchRed;
+        private boolean keepGoing = true;
+
+        public JetStreamReaderImpl(final NatsJetStreamPullSubscription sub, final int batchSize, final int repullAt) {
+            this.sub = sub;
+            this.batchSize = batchSize;
+            this.repullAt = Math.max(1, Math.min(batchSize, repullAt));
+            currentBatchRed = 0;
+            sub.pull(batchSize);
+        }
+
+        @Override
+        public Message nextMessage(Duration timeout) throws InterruptedException, IllegalStateException {
+            return track(sub.nextMessage(timeout));
+        }
+
+        @Override
+        public Message nextMessage(long timeoutMillis) throws InterruptedException, IllegalStateException {
+            return track(sub.nextMessage(timeoutMillis));
+        }
+
+        private Message track(Message msg) {
+            if (msg != null) {
+                if (++currentBatchRed == repullAt) {
+                    if (keepGoing) {
+                        sub.pull(batchSize);
+                    }
+                }
+                if (currentBatchRed == batchSize) {
+                    currentBatchRed = 0;
+                }
+            }
+            return msg;
+        }
+
+        @Override
+        public void stop() {
+            keepGoing = false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JetStreamReader reader(final int batchSize, final int repullAt) {
+        return new JetStreamReaderImpl(this, batchSize, repullAt);
     }
 }
