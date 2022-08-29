60a4d0103cd1089c39728c5afa9b004ab093fe03
scottf <scottfauerbach@gmail.com>
2022-08-29 16:11:27 -0400

    spelling review

--- src/main/java/io/nats/client/ObjectStore.java
+++ src/main/java/io/nats/client/ObjectStore.java
@@ -10,8 +10,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package io.nats.client;
 
+package io.nats.client;
 
 import io.nats.client.api.*;
 import io.nats.client.impl.NatsObjectStoreWatchSubscription;
