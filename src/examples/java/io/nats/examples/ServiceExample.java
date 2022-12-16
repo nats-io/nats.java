61007bf09724c74b411eed00155b6cbb00cd3dff
scottf <scottfauerbach@gmail.com>
2022-12-16 15:13:41 -0500

    service example proper packaging

--- src/examples/java/io/nats/examples/ServiceExample.java
+++ src/examples/java/io/nats/examples/ServiceExample.java
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package io.nats.examples.jetstream;
+package io.nats.examples;
 
 import io.nats.client.*;
 import io.nats.client.support.JsonSerializable;
