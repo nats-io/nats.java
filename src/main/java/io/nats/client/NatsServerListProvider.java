a14960fe01027c759dba28c2c89188d294fb49e0
scottf <scottfauerbach@gmail.com>
2023-02-13 19:19:56 -0500

    re-check logic

--- src/main/java/io/nats/client/NatsServerListProvider.java
+++ src/main/java/io/nats/client/NatsServerListProvider.java
@@ -25,11 +25,39 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class NatsServerListProvider implements ServerListProvider {
 
-    // behavior is to
-    // 1. add the configured servers on construction
-    // 2. optionally add the discovered servers (options default is to include them)
-    // 3. optionally randomize the servers (options default is to randomize)
-    // 4. on randomize, move the current server to the bottom
+    // SERVER LOGIC
+    // 1. On creation, call initialize
+    // 2. Connect calls getServerList and tries the whole list once
+    //    2.1 Just before trying an entry, call resolveHostToIps and actually loop through that list
+    //    2.2. on a successful connection, call acceptDiscoveredUrls
+    // 3. ReConnect calls getServerList and tries the whole list options.maxRetries times
+    //    3.1 Just before trying an entry, call resolveHostToIps and actually loop through that list
+    //    3.2. on a successful connection, call acceptDiscoveredUrls
+
+    // PROVIDER LOGIC
+    // initialize - is only called once to make sure the provider has the Options
+    // 1. load explicit (options) servers, no dupes
+
+    // acceptDiscoveredUrls - is called every time the connection has new server info
+    // 1. copy the internal list (lastList), so we can see if there are any new ones added
+    // 2. reset list to the explicit, no dupes
+    // 3. add discovered (implicit), no dupes, checking if a non-dupe was not in lastList
+    // 4. return true if any discovered where not in the lastList
+
+    // getServerList - is called in both connect and reconnect
+    // 1. if there is a lastConnectedServer, remove it from the internal list
+    // 2. if there are still more than 1 in the internal list and NOT options.noRandomize
+    //    2.1 shuffle list
+    // 3. if there is a lastConnectedServer, add it to the end of the list
+    // 4. return list
+
+    // resolveHostToIps - is called by connect/reconnect looping through the getServerList
+    // 1. make a list to hold resolved (rlist)
+    // 1. if options.resolveHostnames
+    //    1.1 try to resolve the hostname, adding to rlist
+    // 2. if rlist is empty (either since NOT options.resolveHostnames or resolution didn't resolve anything)
+    //    2.1 put the original hostname into rlist
+    // 3. return rlist
 
     private Options options;
     private final List<NatsUri> list;
@@ -40,19 +68,35 @@ public class NatsServerListProvider implements ServerListProvider {
 
     public void initialize(Options opts) {
         this.options = opts;
+        reset();
+    }
+
+    private void reset() {
+        list.clear();
         for (NatsUri nuri : options.getNatsServerUris()) {
-            add(nuri);
+            addNoDupes(nuri);
         }
     }
 
     @Override
     public boolean acceptDiscoveredUrls(List<String> discoveredServers) {
+        if (options.isIgnoreDiscoveredServers()) {
+            // never want anything but the explicit, which is already loaded.
+            return false; // there are no new servers
+        }
+
+        List<NatsUri> lastList = new ArrayList<>(list); // copy to test for changes
+
+        reset();
+
         boolean anyAdded = false;
         if (!options.isIgnoreDiscoveredServers()) {
-            // TODO PRUNE ???
             for (String discovered : discoveredServers) {
                 try {
-                    anyAdded |= add(new NatsUri(discovered));
+                    NatsUri nuri = new NatsUri(discovered);
+                    if (addNoDupes(nuri) && !anyAdded) {
+                        anyAdded = !lastList.contains(nuri);
+                    }
                 }
                 catch (URISyntaxException ignore) {
                     // should never actually happen
@@ -64,17 +108,14 @@ public class NatsServerListProvider implements ServerListProvider {
 
     @Override
     public List<NatsUri> getServerList(NatsUri lastConnectedServer) {
-        if (list.size() > 1) {
-            boolean removed = false;
-            if (lastConnectedServer != null) {
-                removed = list.remove(lastConnectedServer);
-            }
-            if (!options.isNoRandomize() && list.size() > 1) {
-                Collections.shuffle(list, ThreadLocalRandom.current());
-            }
-            if (removed) {
-                list.add(lastConnectedServer);
-            }
+        if (lastConnectedServer != null) {
+            list.remove(lastConnectedServer);
+        }
+        if (list.size() > 1 && !options.isNoRandomize()) {
+            Collections.shuffle(list, ThreadLocalRandom.current());
+        }
+        if (lastConnectedServer != null) {
+            list.add(lastConnectedServer);
         }
         return new ArrayList<>(list); // return a copy
     }
@@ -101,7 +142,7 @@ public class NatsServerListProvider implements ServerListProvider {
         return resolved;
     }
 
-    private boolean add(NatsUri nuri) {
+    private boolean addNoDupes(NatsUri nuri) {
         if (list.contains(nuri)) {
             return false;
         }
