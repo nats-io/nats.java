5683a99fd72285a12f0efe923cf94fb0b2b3e14b
scottf <scottfauerbach@gmail.com>
2023-03-18 05:49:41 -0400

    errors vs warnings vs ignores

--- src/main/java/io/nats/client/support/PullStatus.java
+++ src/main/java/io/nats/client/support/PullStatus.java
@@ -35,13 +35,4 @@ public class PullStatus {
     public boolean isTrackingHeartbeats() {
         return trackingHeartbeats;
     }
-
-    @Override
-    public String toString() {
-        return "PullStatus{" +
-            "pendingMessages=" + pendingMessages +
-            ", pendingBytes=" + pendingBytes +
-            ", trackingHeartbeats=" + trackingHeartbeats +
-            '}';
-    }
 }
