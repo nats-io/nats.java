8307d95f4cc5f1eb6b515ba3cd35fce7eae57fef
scottf <scottfauerbach@gmail.com>
2025-08-11 06:33:58 -0400

    remove debug

--- src/main/java/io/nats/client/impl/NatsKeyValue.java
+++ src/main/java/io/nats/client/impl/NatsKeyValue.java
@@ -16,7 +16,6 @@ package io.nats.client.impl;
 import io.nats.client.*;
 import io.nats.client.api.*;
 import io.nats.client.support.DateTimeUtils;
-import io.nats.client.support.Debug;
 import io.nats.client.support.Validator;
 
 import java.io.IOException;
@@ -112,7 +111,6 @@ public class NatsKeyValue extends NatsFeatureBase implements KeyValue {
     }
 
     KeyValueEntry existingOnly(KeyValueEntry kve) {
-        Debug.info("_get", kve);
         return kve == null || kve.getOperation() != KeyValueOperation.PUT ? null : kve;
     }
 
