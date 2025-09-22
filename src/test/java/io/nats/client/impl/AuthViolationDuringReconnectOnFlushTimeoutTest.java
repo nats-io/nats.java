b6efce21ccefea3e3cf4320777203e39961efaac
Yevhen Surovskyi <surovskyi.y@ajax.systems>
2025-09-22 12:10:28 +0300

    Test to reproduce #1426

--- src/test/java/io/nats/client/impl/AuthViolationDuringReconnectOnFlushTimeoutTest.java
+++ src/test/java/io/nats/client/impl/AuthViolationDuringReconnectOnFlushTimeoutTest.java
@@ -14,38 +14,23 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /* Test to reproduce #1426 */
 public class AuthViolationDuringReconnectOnFlushTimeoutTest {
+
+    private static final int NUMBER_OF_SUBS = 1_000_000;
 
     private static void startServer(Context ctx) throws IOException {
         ctx.server.set(new NatsTestServer(new String[]{"--auth", "1234"}, ctx.port, false));
     }
 
-    private static Runnable waitCloseSocket(Context ctx) {
-        return () -> {
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            while (!ctx.violated.get() && ctx.restartsLeft.get() >= 0) {
-                if (ctx.nc.closeSocketLock.isLocked()) {
-                    ctx.latch.countDown();
-                    // just acquire the lock and release it
-                    try {
-                        ctx.nc.closeSocketLock.lock();
-                    } finally {
-                        ctx.nc.closeSocketLock.unlock();
-                    }
-                }
-            }
-        };
-    }
-
     private static void restartServer(Context ctx) {
         try {
-            ctx.restartsLeft.decrementAndGet();
+            if (ctx.restartsLeft.getCount() == 0) {
+                return;
+            }
+            ctx.restartsLeft.countDown();
             ctx.server.get().shutdown();
             startServer(ctx);
         } catch (Exception e) {
@@ -63,35 +48,43 @@ public class AuthViolationDuringReconnectOnFlushTimeoutTest {
                     .servers(new String[]{"nats://" + "127.0.0.1:" + ctx.port})
                     .noRandomize()
                     .token(new char[]{'1', '2', '3', '4'})
-                    .bufferSize(10)
-                    .dataPortType(SlowSocketDataPort.class.getCanonicalName())
-                    .reconnectBufferSize(10)
-                    .reconnectWait(Duration.ofMillis(1000))
+
+                    .maxMessagesInOutgoingQueue(NUMBER_OF_SUBS )
+                    .reconnectBufferSize(NUMBER_OF_SUBS * 100)
+                    .connectionTimeout(Duration.ofMillis(10))
+                    .reconnectWait(Duration.ofMillis(2000))
                     .connectionListener((conn, e) ->
                             System.out.println(String.format("Tid: %d, NATS: connection event - %s, connected url: %s. servers: %s ", Thread.currentThread().getId(), e, conn.getConnectedUrl(), conn.getServers())
                             ))
                     .errorListener(ctx.errorListener)
                     .build();
 
-            ctx.nc = new MockPausingNatsConnection(options);
+            ctx.nc = new NatsConnection(options);
             ctx.nc.connect(true);
-            ctx.d = ctx.nc.createDispatcher(m -> {
-            });
+            CountDownLatch processedMessages = new CountDownLatch(NUMBER_OF_SUBS);
+            ctx.d = ctx.nc.createDispatcher();
 
-            ctx.latch = new CountDownLatch(1);
-            for (int i = 0; i < 1_000; i++) {
+            for (int i = 0; i < NUMBER_OF_SUBS; i++) {
                 String subject = "test_" + i;
                 ctx.subscriptions.add(subject);
-                ctx.d.subscribe(subject);
+                ctx.d.subscribe(subject, "q", m -> processedMessages.countDown());
             }
 
-            ctx.serverRestarter.scheduleWithFixedDelay(() -> restartServer(ctx), 2000, 5000, TimeUnit.MILLISECONDS);
+            ctx.serverRestarter.scheduleWithFixedDelay(() -> restartServer(ctx), 2, 20, TimeUnit.SECONDS);
 
-            Thread t = new Thread(waitCloseSocket(ctx));
-            t.start();
-            t.join();
+            ctx.restartsLeft.await();
+            TimeUnit.SECONDS.sleep(20); // give time to restore all subscriptions
 
+            synchronized(ctx.nc.getStatus()) {
+                while (ctx.nc.getStatus() != Connection.Status.CONNECTED && ctx.nc.getStatus() != Connection.Status.CLOSED) {
+                }
+            }
             assertFalse(ctx.violated.get());
+
+
+            ctx.subscriptions.forEach(s -> ctx.nc.publish(s, "1".getBytes()));
+
+            assertTrue(processedMessages.await(10, TimeUnit.SECONDS), "cdl " + processedMessages.getCount());
         }
     }
 
@@ -99,12 +92,11 @@ public class AuthViolationDuringReconnectOnFlushTimeoutTest {
         int port;
         NatsConnection nc;
         Dispatcher d;
-        CountDownLatch latch;
         ConcurrentHashMap.KeySetView<String, Boolean> subscriptions = ConcurrentHashMap.newKeySet();
         ScheduledExecutorService serverRestarter = Executors.newSingleThreadScheduledExecutor();
         AtomicReference<NatsTestServer> server = new AtomicReference<>();
         AtomicBoolean violated = new AtomicBoolean(false);
-        AtomicInteger restartsLeft = new AtomicInteger(3);
+        CountDownLatch restartsLeft = new CountDownLatch(2);
         ErrorListener errorListener = new ErrorListener() {
             @Override
             public void slowConsumerDetected(Connection conn, Consumer consumer) {
@@ -131,38 +123,6 @@ public class AuthViolationDuringReconnectOnFlushTimeoutTest {
         public void close() throws Exception {
             serverRestarter.shutdown();
             server.get().shutdown();
-        }
-    }
-
-    static class MockPausingNatsConnection extends NatsConnection {
-        private boolean reconnectFlow = false;
-
-        MockPausingNatsConnection(@NonNull Options options) {
-            super(options);
-        }
-
-        @Override
-        void handleCommunicationIssue(Exception io)  {
-            reconnectFlow = true;
-            super.handleCommunicationIssue(io);
-        }
-
-        @Override
-        void tryToConnect(NatsUri cur, NatsUri resolved, long now) {
-            super.tryToConnect(cur, resolved, now);
-            if (reconnectFlow && isConnected()) {
-                SlowSocketDataPort.enabledPause.set(true);
-            }
-        }
-
-        @Override
-        public void flush(@Nullable Duration timeout) throws TimeoutException, InterruptedException {
-            try {
-                super.flush(timeout);
-            } finally {
-                reconnectFlow = false;
-                SlowSocketDataPort.enabledPause.set(false);
-            }
         }
     }
 
