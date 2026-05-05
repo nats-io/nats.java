8cc5a135e1405685746c0bcb4bec8962e88d8a34
scottf <scottfauerbach@gmail.com>
2026-05-05 14:15:39 -0400

    removed debug

--- src/main/java/io/nats/client/impl/NatsJetStreamImpl.java
+++ src/main/java/io/nats/client/impl/NatsJetStreamImpl.java
@@ -129,7 +129,6 @@ class NatsJetStreamImpl implements NatsJetStreamConstants {
         }
 
         ConsumerCreateRequest ccr = new ConsumerCreateRequest(streamName, config, action);
-        System.out.println(subj + " -> " + ccr.toJson());
         Message resp = makeRequestResponseRequired(subj, ccr.serialize(), getTimeout());
         return new ConsumerInfo(resp).throwOnHasError();
     }
